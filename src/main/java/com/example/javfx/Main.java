80632ec42892e5a1387527fbde8874f4f3397c4f
IliasElQ <60800392+IliasElQ@users.noreply.github.com>
2023-08-25 20:10:02 +0100

    Update Main.java

--- src/main/java/com/example/javfx/Main.java
+++ src/main/java/com/example/javfx/Main.java
@@ -8,48 +8,45 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-
-
-
-
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
         try {
-
-            //Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+            // Chargement de la vue de la page de connexion
             Parent root = FXMLLoader.load(getClass().getResource("Login-page.fxml"));
             Scene scene = new Scene(root);
+
+            // Définition de la scène dans la fenêtre principale
             stage.setScene(scene);
             stage.show();
+
+            // Gestionnaire de fermeture de la fenêtre
             stage.setOnCloseRequest(event -> {
-                event.consume();
-                logout(stage);
-
+                event.consume(); // Empêche la fermeture automatique
+                logout(stage);   // Appelle la méthode de déconnexion personnalisée
             });
-
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    // Méthode pour la déconnexion
     public void logout(Stage stage) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Logout");
-        alert.setHeaderText("You are about to logout!");
-        alert.setContentText("Do you want to save before exiting");
+        alert.setTitle("Déconnexion");
+        alert.setHeaderText("Vous êtes sur le point de vous déconnecter !");
+        alert.setContentText("Voulez-vous enregistrer avant de quitter ?");
 
+        // Attente de la réponse de l'utilisateur
         if (alert.showAndWait().get() == ButtonType.OK) {
-            System.out.println("You successfully logged out");
-            stage.close();
+            System.out.println("Vous avez été déconnecté avec succès.");
+            stage.close(); // Fermeture de la fenêtre
         }
     }
 
-    public static  void main(String[] args){
-        launch(args);
-
+    public static void main(String[] args){
+        launch(args); // Lance l'application JavaFX
     }
-
 }
